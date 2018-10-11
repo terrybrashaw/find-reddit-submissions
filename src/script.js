6e382e0ba59c03c2fea1e7932824800af5b7ab0f
Terry Brashaw <terrybrashaw@gmail.com>
2018-10-10 23:16:28 -0400

    Add multiple alternative URL queries
    Querying Reddit for matching URLs is explicit; "https://youtube.com" is
    not equal to "youtube.com".

--- src/script.js
+++ src/script.js
@@ -1,34 +1,51 @@
-browser.tabs.query({active: true, currentWindow: true}).then(current_tab => {
-    let url = current_tab[0].url;
-
+async function get_posts() {
+    let current_tab = await browser.tabs.query({active: true, currentWindow: true});
+    let url = new URL(current_tab[0].url);
     let urls = [
-        url.trim
+        `${url.host}${url.pathname}${url.search}`,
+        `https://${url.host}${url.pathname}${url.search}`,
+        `http://${url.host}${url.pathname}${url.search}`,
     ];
 
-    let encoded_url = encodeURIComponent(url);
-    let request_url = `https://www.reddit.com/api/info/.json?raw_json=1&url=${encoded_url}`;
-    let request = {
-        method: "GET",
-        headers: new Headers({
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-        }),
-    };
+    let responses = [];
+    for(let i = 0; i < urls.length; i++) {
+        let encoded_url = encodeURIComponent(urls[i]);
+        let request_url = `https://www.reddit.com/api/info/.json?raw_json=1&url=${encoded_url}`;
+        let request = {
+            method: "GET",
+            headers: new Headers({
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+            }),
+        };
+        let response = fetch(request_url, request);
+        responses.push(response);
+    }
 
-    let response = fetch(request_url, request)
-        .then(response => response.json());
+    let posts = [];
+    for(let i = 0; i < responses.length; i++) {
+        let response = await (await responses[i]).json();
+        console.log(response);
+        for(let i = 0; i < response.data.children.length; i++) {
+            posts.push(response.data.children[i]);
+        }
+    }
 
-    return response;
-}).then(response => {
-    if(response.data.children.length == 0) {
+    return posts;
+}
+
+async function render_posts(posts) {
+    console.log(posts);
+
+    if(posts.length == 0) {
         document.getElementById("error-content").style.display = "block";
         document.getElementById("popup-content").style.display = "none";
         return;
     }
-    response.data.children.sort((a, b) => b.data.num_comments - a.data.num_comments);
-    let posts = [];
-    for(let i = 0; i < response.data.children.length; i++) {
-        let post = response.data.children[i].data;
+    posts.sort((a, b) => b.data.num_comments - a.data.num_comments);
+    let rendered_posts = [];
+    for(let i = 0; i < posts.length; i++) {
+        let post = posts[i].data;
         let author;
         if(post.author === "[deleted]") {
             author = "[deleted]";
@@ -49,7 +66,9 @@ browser.tabs.query({active: true, currentWindow: true}).then(current_tab => {
             </div>
         </a>
         `;
-        posts.push(html);
+        rendered_posts.push(html);
     }
-    document.body.innerHTML = posts.join("\n");
-});
+    document.body.innerHTML = rendered_posts.join("\n");
+}
+
+get_posts().then(render_posts);
